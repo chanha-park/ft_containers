a723c590d585d271a4e327ce3992978c37b216bc
chanhpar <chanhpar@student.42seoul.kr>
2022-12-23 21:05:28 +0900

    tree__.hpp: WIP - implement nested class outside the rb_tree__ class

--- tree__.hpp
+++ tree__.hpp
@@ -6,7 +6,7 @@
 /*   By: chanhpar <chanhpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 22:07:57 by chanhpar          #+#    #+#             */
-/*   Updated: 2022/12/23 02:54:58 by chanhpar         ###   ########.fr       */
+/*   Updated: 2022/12/23 12:05:46 by chanhpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,24 +30,35 @@ class rb_tree__ {
   typedef Compare key_compare;
 
  private:
-  struct rb_tree_node_base__ {
-    rb_tree_node_base__* parent;
-    rb_tree_node_base__* left;
-    rb_tree_node_base__* right;
-    bool isRed;
-  };
-
-  struct rb_tree_node_value__ : public rb_tree_node_base__ {
-    value_type val;
-  };
-
-  struct rb_tree_alloc_base__ {};
-
-  struct rb_tree_node__ : public rb_tree_alloc_base__ {};
+  struct rb_tree_node_base__;
+  struct rb_tree_node_value__;
+  struct rb_tree_alloc_base__;
+  struct rb_tree_node__;
 
  protected:
  public:
+};  // class rb_tree__
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+struct rb_tree__<Key, T, Compare, Allocator>::rb_tree_node_base__ {
+  rb_tree_node_base__* parent;
+  rb_tree_node_base__* left;
+  rb_tree_node_base__* right;
+  bool isRed;
 };
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+struct rb_tree__<Key, T, Compare, Allocator>::rb_tree_node_value__ :
+    public rb_tree_node_base__ {
+  value_type val;
+};
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+struct rb_tree__<Key, T, Compare, Allocator>::rb_tree_alloc_base__ {};
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+struct rb_tree__<Key, T, Compare, Allocator>::rb_tree_node__ :
+    public rb_tree_alloc_base__ {};
 
 }  // namespace ft
 
