a75051e68ee9dd4f08c59ca92ab26da07e55a517
chanha-park <chanhpar@student.42seoul.kr>
2022-12-27 18:16:36 +0900

    tree__.hpp: Make rb_tree_node_base__. Move implementations inside the class

--- tree__.hpp
+++ tree__.hpp
@@ -6,7 +6,7 @@
 /*   By: chanhpar <chanhpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 22:07:57 by chanhpar          #+#    #+#             */
-/*   Updated: 2022/12/23 12:05:46 by chanhpar         ###   ########.fr       */
+/*   Updated: 2022/12/27 18:15:23 by chanhpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,35 +30,64 @@ class rb_tree__ {
   typedef Compare key_compare;
 
  private:
-  struct rb_tree_node_base__;
-  struct rb_tree_node_value__;
-  struct rb_tree_alloc_base__;
-  struct rb_tree_node__;
+  struct rb_tree_node_base__ {
+    // struct rb_tree_node_base__ {{{
+
+    rb_tree_node_base__* parent;
+    rb_tree_node_base__* left;
+    rb_tree_node_base__* right;
+    bool isRed;
+
+    rb_tree_node_base__*
+    find_next_node__(void) const {
+      rb_tree_node_base__* curr = this;
+      if (curr->right) {
+        curr = curr->right;
+        while (curr->left)
+          curr = curr->left;
+      } else {
+        while (curr == curr->parent->right)
+          curr = curr->parent;
+        if (curr->parent != curr->right)
+          curr = curr->parent;
+      }
+      return (curr);
+    }
+
+    rb_tree_node_base__*
+    find_prev_node__(void) const {
+      rb_tree_node_base__* curr = this;
+      if (isRed && curr->parent->parent == curr) {
+        curr = curr->right;
+      } else if (curr->left) {
+        curr = curr->left;
+        while (curr->right)
+          curr = curr->right;
+
+      } else {
+        while (curr == curr->parent->left)
+          curr = curr->parent;
+        curr = curr->parent;
+      }
+      return (curr);
+    }
+
+    // struct rb_tree_node_base__ }}}
+  };
+
+  struct rb_tree_node_value__ : public rb_tree_node_base__ {
+    value_type val;
+  };
+
+  struct rb_tree_iterator__ {};
+
+  struct rb_tree_alloc_base__ {};
+
+  struct rb_tree_node__ : public rb_tree_alloc_base__ {};
 
  protected:
  public:
 };  // class rb_tree__
-
-template <typename Key, typename T, typename Compare, typename Allocator>
-struct rb_tree__<Key, T, Compare, Allocator>::rb_tree_node_base__ {
-  rb_tree_node_base__* parent;
-  rb_tree_node_base__* left;
-  rb_tree_node_base__* right;
-  bool isRed;
-};
-
-template <typename Key, typename T, typename Compare, typename Allocator>
-struct rb_tree__<Key, T, Compare, Allocator>::rb_tree_node_value__ :
-    public rb_tree_node_base__ {
-  value_type val;
-};
-
-template <typename Key, typename T, typename Compare, typename Allocator>
-struct rb_tree__<Key, T, Compare, Allocator>::rb_tree_alloc_base__ {};
-
-template <typename Key, typename T, typename Compare, typename Allocator>
-struct rb_tree__<Key, T, Compare, Allocator>::rb_tree_node__ :
-    public rb_tree_alloc_base__ {};
 
 }  // namespace ft
 
