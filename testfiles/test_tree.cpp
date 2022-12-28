4fcde08ad1c05fd1cab7d9b4b76c4ed836a581be
chanha-park <chanhpar@student.42seoul.kr>
2022-12-28 18:03:23 +0900

    Add test_tree.cpp

---
+++ testfiles/test_tree.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "tree__.hpp"
+
+int
+main(void) {
+  ft::rb_tree__<int, ft::pair<int, std::string>, std::less<int> > tr;
+  (void)tr;
+  return (0);
+}
