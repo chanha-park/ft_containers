52df2c61225074c0553b0cf49bae4b1e2f861a99
chanha-park <chanhpar@student.42seoul.kr>
2022-12-28 18:03:35 +0900

    WIP: tree__.hpp
    1. rb_tree_node_base__ has virtual destructor
    2. Add more typedefs to rb_tree__
    3. Make rb_tree_iterator__, typedef iter, typedef const_iterator

--- tree__.hpp
+++ tree__.hpp
@@ -6,7 +6,7 @@
 /*   By: chanhpar <chanhpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 22:07:57 by chanhpar          #+#    #+#             */
-/*   Updated: 2022/12/27 18:15:23 by chanhpar         ###   ########.fr       */
+/*   Updated: 2022/12/28 18:02:18 by chanhpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,19 @@
 #define FT_CONTAINERS_TREE_HPP
 
 #include <memory>
+#include "iterator.hpp"
+#include "memory.hpp"
 #include "utility.hpp"
 
 namespace ft {
 
 template <typename Key,
-          typename T,
+          typename Value,
           typename Compare,
-          typename Allocator = std::allocator<T> >
+          typename Allocator = std::allocator<Value> >
 class rb_tree__ {
- public:
-  typedef Key key_type;
-  typedef T mapped_type;
-  typedef typename ft::pair<const key_type, mapped_type> value_type;
-  typedef Compare key_compare;
-
  private:
+  // XXX consider virtual?
   struct rb_tree_node_base__ {
     // struct rb_tree_node_base__ {{{
 
@@ -72,14 +69,59 @@ class rb_tree__ {
       return (curr);
     }
 
+    virtual ~rb_tree_node_base__(void) {
+    }
+
     // struct rb_tree_node_base__ }}}
   };
 
+ public:
+  typedef Key key_type;
+  typedef Value value_type;
+  typedef Compare key_compare;
+  typedef ptrdiff_t difference_type;
+  typedef value_type* pointer;
+  typedef const value_type* const_pointer;
+  typedef value_type& reference;
+  typedef const value_type& const_reference;
+
+ private:
+  template <typename T>
   struct rb_tree_node_value__ : public rb_tree_node_base__ {
-    value_type val;
+    T val;
   };
 
-  struct rb_tree_iterator__ {};
+  typedef rb_tree_node_base__ base_node__;
+  typedef rb_tree_node_value__<value_type> value_node__;
+
+  template <typename T>
+  struct rb_tree_iterator__ {
+    typedef ft::bidirectional_iterator_tag iterator_category;
+    base_node__* node__;
+
+    rb_tree_iterator__(void) {
+    }
+
+    rb_tree_iterator__(rb_tree_node_value__<T>* x) :
+        node__(static_cast<base_node__*>(x)) {
+    }
+
+    rb_tree_iterator__(const rb_tree_iterator__& it) : node__(it.node__) {
+    }
+
+    reference
+    operator*() const {
+      return (dynamic_cast<rb_tree_node_value__<T>*>(node__)->val);
+    }
+
+    pointer
+    operator->() const {
+      return (ft::addressof(operator*()));
+    }
+  };
+
+  typedef rb_tree_iterator__<value_type> iterator;
+  typedef rb_tree_iterator__<const value_type> const_iterator;
 
   struct rb_tree_alloc_base__ {};
 
@@ -87,8 +129,14 @@ class rb_tree__ {
 
  protected:
  public:
+  rb_tree_node_base__ dummy1;
+  rb_tree_node_value__<value_type> dummy2;
+  iterator dummy3;
+  rb_tree_alloc_base__ dummy4;
+  rb_tree_node__ dummy5;
+
 };  // class rb_tree__
 
 }  // namespace ft
 
-#endif
+#endif  // FT_CONTAINERS_TREE_HPP
