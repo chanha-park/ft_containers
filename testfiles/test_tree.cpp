390e620916857b30b6d0cd0d70f86b4ed4730378
chanha-park <chanhpar@student.42seoul.kr>
2022-12-29 13:20:50 +0900

    Update testfiles/test_tree.hpp

--- testfiles/test_tree.cpp
+++ testfiles/test_tree.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <string>
-#include "tree__.hpp"
+#include "../tree__.hpp"
 
 int
 main(void) {
   ft::rb_tree__<int, ft::pair<int, std::string>, std::less<int> > tr;
+  ft::rb_tree__<int, ft::pair<int, std::string>, std::less<int> >::iterator it;
+  ft::rb_tree__<int, ft::pair<int, std::string>, std::less<int> >::
+      const_iterator cit;
+  ft::rb_tree__<int, ft::pair<int, std::string>, std::less<int> >::
+      reverse_iterator rit;
+  ft::rb_tree__<int, ft::pair<int, std::string>, std::less<int> >::
+      const_reverse_iterator crit;
   (void)tr;
+  (void)it;
+  (void)cit;
+  (void)rit;
+  (void)crit;
   return (0);
 }
